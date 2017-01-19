9f4c9f05d7a18053ae5257010d73ffc0c261d990
AD Mohanraj <admohanraj@airbears2-10-142-61-237.airbears2.1918.berkeley.edu>
2017-01-18 18:22:02 -0800

    Initial Commit

---
+++ BestResponse/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BestResponse
+//
+//  Created by AD Mohanraj on 1/18/17.
+//  Copyright © 2017 AD Mohanraj. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
