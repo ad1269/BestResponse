dee31d8325aef9ba06ee1a6610b8c14cbce5f0f6
AD Mohanraj <admohanraj@airbears2-10-142-61-105.airbears2.1918.berkeley.edu>
2017-01-19 17:20:25 -0800

    Added more flexible TableViewController

---
+++ BestResponse/TableFeedViewController.swift
@@ -0,0 +1,14 @@
+//
+//  TableFeedViewController.swift
+//  BestResponse
+//
+//  Created by AD Mohanraj on 1/19/17.
+//  Copyright © 2017 AD Mohanraj. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class TableFeedViewController: UITableViewController {
+    
+}
