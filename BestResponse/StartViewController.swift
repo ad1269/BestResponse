1dcd16a008918eca69349d970fa291d78e183d79
AD Mohanraj <admohanraj@airbears2-10-142-62-130.airbears2.1918.berkeley.edu>
2017-01-22 18:08:15 -0800

    Set up basic login and signup interface.

---
+++ BestResponse/StartViewController.swift
@@ -0,0 +1,47 @@
+//
+//  StartViewController.swift
+//  BestResponse
+//
+//  Created by AD Mohanraj on 1/22/17.
+//  Copyright © 2017 AD Mohanraj. All rights reserved.
+//
+
+import UIKit
+
+class StartViewController: UIViewController {
+
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var signupButton: UIButton!
+    @IBOutlet weak var usernameField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func login(_ sender: Any) {
+    
+    }
+    
+    @IBAction func signup(_ sender: Any) {
+    
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
